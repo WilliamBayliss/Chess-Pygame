1016b31a705a8074094c3f754554cfb09f6f556f
William Bayliss <wmbayliss@gmail.com>
2026-05-28 15:13:30 -0400

    Move board.py changes into correct file, add pygame elements to chess.py

--- chess.py
+++ chess.py
@@ -4,5 +4,18 @@ from lib.piece import Piece
 from lib.player import Player
 
 pygame.init()
+WIDTH, HEIGHT = 400, 400
+SQUARE_SIZE = WIDTH // 8
 
-screen = pygame.display.set_mode((800, 600))
+
+board = Board()
+
+screen = pygame.display.set_caption("Chessboard")
+
+# Colours, light is offwhite, dark is brown
+LIGHT = (245, 245, 229) 
+DARK = (139, 69, 19)
+
+def draw_board(board):
+    
+
--- lib/board.py
+++ lib/board.py
@@ -1,14 +1,35 @@
+from piece import Piece
+class Square:
+    __init__(self):
+        self.row_coordinate = None
+        self.column_coordinate = None
+        self.colour = None
+
 class Board:
     __init__(self):
         self.board = create_board(self)
-
     
+    def paint_board(board):
+        for row in board:
+            if row[0].row_coordinate % 2 = 0:
+                row[0:2:4:6].colour = 0
+                row[1:3:5:7].colour = 1
+            else:
+                row[0:2:4:6].colour = 1
+                row[1:3:5:7].colour = 0
+            
+
+
     def create_board(self):
         board = []
         for letter in range(ord('A'), ord('Z') + 1):
             row = []
-            for number in range(0, 7):
-                row.append([letter, number])
+            for number in range(1, 8):
+                square = Square()
+                square.row_coordinate = number
+                square.column_coordinate = letter
+                row.append(square)
             board.append(row)
+        paint_board(board)
         return board
-            
+
