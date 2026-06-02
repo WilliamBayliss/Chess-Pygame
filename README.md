8f7f36d28fe5efb43d67fb38a1921f4b605078e4
William Bayliss <wmbayliss@gmail.com>
2026-06-02 14:13:26 -0400

    Added chess pieces from creative commons, added README.md with link to creative commons license and author name

---
+++ README.md
@@ -0,0 +1,3 @@
+Chess piece art was retreived from the Chesscraft Creative Commons on Gitlab by author Stuart Spence, under a creative commons license found here:
+https://gitlab.com/zulban/chesscraft-creative-commons/-/blob/master/LICENSE?ref_type=heads
+
--- chess.py
+++ chess.py
@@ -40,10 +40,13 @@ class Board:
 class Piece:
     def __init__(self):
         self.square = [None, None]
-        self.color = None
+        self.colour = None
         self.sprite = None
         self.moved = False
         self.available_moves = None
+
+    def set_colour(self, player):
+        self.colour = player.colour
 
 
 
